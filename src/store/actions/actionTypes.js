14f0835a23f11ad08b7ec7eb8a421aae7b3dcb06
Umer Mustafa <umer.mustafa789@gmail.com>
2023-05-20 00:22:26 -0400

    Adding Actions for reducers

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,22 @@
+// Action Types
+
+// Action type for fetching all employees
+export const FETCH_ALL_EMPLOYEES = "FETCH_ALL_EMPLOYEES";
+
+// Action type for fetching a single employee
+export const FETCH_EMPLOYEE = "FETCH_EMPLOYEE";
+
+// Action type for fetching all tasks
+export const FETCH_ALL_TASKS = "FETCH_ALL_TASKS";
+
+// Action type for fetching a single task
+export const FETCH_TASK = "FETCH_TASK";
+
+// Action type for adding a task
+export const ADD_TASK = "ADD_TASK";
+
+// Action type for deleting a task
+export const DELETE_TASK = "DELETE_TASK";
+
+// Action type for editing a task
+export const EDIT_TASK = "EDIT_TASK";
